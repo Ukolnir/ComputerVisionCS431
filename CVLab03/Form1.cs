5ba0b027371b7fdc86f4452ca19efca7cfd84286
Ukolnir <metallwind@gmail.com>
2020-03-09 13:45:46 +0300

    added operations

--- CVLab03/Form1.cs
+++ CVLab03/Form1.cs
@@ -17,27 +17,72 @@ namespace CVLab03
 			InitializeComponent();
 		}
 
-		Bitmap sourceFile;
+        Bitmap sourceFile;
 
-		private void loadPictureToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			if (openFileDialog1.ShowDialog() == DialogResult.OK)
-			{
-				//допустим, что на входе уже бинаризованное изображение
-				sourceFile = new Bitmap(openFileDialog1.FileName);
-				pictureBox1.Image = sourceFile;
+        private void loadPictureToolStripMenuItem_Click(object sender, EventArgs e){
+			if (openFileDialog1.ShowDialog() == DialogResult.OK){
+                sourceFile = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = sourceFile;
 			}
 		}
 
-		private void savePictureToolStripMenuItem_Click(object sender, EventArgs e)
-		{
+		private void savePictureToolStripMenuItem_Click(object sender, EventArgs e){
 			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
 				pictureBox2.Image.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
 		}
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			//Сделать поэтапное изменение картинки
-		}
-	}
+        List<string> maskNames;
+
+        private void loadMaskToolStripMenuItem_Click(object sender, EventArgs e){
+            maskNames = new List<string>();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                maskNames = openFileDialog1.FileNames.ToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e){
+            var b = Operations.BitmapToArr(sourceFile);
+            var b1 = Operations.Erode(b,
+                    Operations.BitmapToArr(new Bitmap(maskNames[0])));
+
+            var b2 = Operations.Dilate(b1,
+                    Operations.BitmapToArr(new Bitmap(maskNames[1])));
+
+            var b3 = Operations.Or(b, b2);
+
+            var b4 = Operations.Opening(b3,
+                Operations.BitmapToArr(new Bitmap(maskNames[2])));
+
+            var b5 = Operations.Dilate(b4,
+                    Operations.BitmapToArr(new Bitmap(maskNames[3])));
+
+            var b6 = Operations.Dilate(b5,
+                    Operations.BitmapToArr(new Bitmap(maskNames[4])));
+
+            var b7 = Operations.Difference(b6, b5);
+
+            var b8 = Operations.And(b, b7);
+
+            var b9 = Operations.Dilate(b8,
+                   Operations.BitmapToArr(new Bitmap(maskNames[5])));
+
+            var b100 = Operations.Difference(b7, b9);
+            var b101 = Operations.Dilate(b100,
+                   Operations.BitmapToArr(new Bitmap(maskNames[6])));
+            var b10 = Operations.Or(b101,b9);
+            pictureBox2.Image = Operations.ArrToBitmap(b10);
+        }
+
+        /*
+        1. Эрозия с маской-кольцо +
+        2. Дилатация с маской-кружком +
+        3. Исходное ИЛИ (2) +
+        4. Размыкание маской-диском с радиусом == шестеренке - высота зубца
+        5. Дилатация с маской-sampling-ring-spacer ????
+        6. Дилатация с маской-sampling-ring-width ???
+        7. (6) - (5)
+        8. Исходное И (7)
+        9. Дилатация с маской-немного расширение полосы
+        10. (Дилатация ((7) - (9)) с маской-кружком для дефектов) ИЛИ (9)
+         */
+    }
 }
--- CVLab03/Operations.cs
+++ CVLab03/Operations.cs
@@ -13,7 +13,7 @@ namespace CVLab03
 			var dest = new int[src.Width, src.Height];
 			for (int x = 0; x < src.Width; ++x)
 				for (int y = 0; y < src.Height; ++y)
-					dest[x, y] = src.GetPixel(x, y).R;
+					dest[x, y] = src.GetPixel(x, y).R == 0? 0 : 1;
 			return dest;
 		}
 
@@ -22,21 +22,89 @@ namespace CVLab03
 			for (int x = 0; x < dest.Width; ++x)
 				for (int y = 0; y < dest.Height; ++y){
 					var t = src[x, y];
-					dest.SetPixel(x, y, Color.FromArgb(t,t,t));
+					dest.SetPixel(x, y, Color.FromArgb(t*255,t*255,t*255));
 				}
 			return dest;
 		}
 
-		public static int[,] Dilation(int[,] src, int[,] mask) {
+		public static int[,] Dilate(int[,] src, int[,] mask) {
 			var W = src.GetLength(0);
 			var H = src.GetLength(1);
 			var dest = new int[W, H];
-			var MW = mask.GetLength(0);
-			var MH = mask.GetLength(1);
-
-
+			var MW = mask.GetLength(0) / 2;
+			var MH = mask.GetLength(1) / 2;
+            for (int y = MH; y < H - MH; ++y)
+                for (int x = MW; x < W - MW; ++x) {
+                    int max = 0;
+                    for (int j = -MH; j < MH; ++j)
+                        for (int i = -MW; i < MW; ++i)
+                            if (mask[i + MW, j + MH] == 1 && src[x + i, y + j] > max)
+                                max = src[x + i, y + j];
+                    dest[x, y] = max;
+                }
 			return dest;
 		}
 
-	}
+        public static int[,] Erode(int[,] src, int[,] mask){
+            var W = src.GetLength(0);
+            var H = src.GetLength(1);
+            var dest = new int[W, H];
+            var MW = mask.GetLength(0) / 2;
+            var MH = mask.GetLength(1) / 2;
+            for (int y = MH; y < H - MH; ++y)
+                for (int x = MW; x < W - MW; ++x)
+                {
+                    int min = Int32.MaxValue;
+                    for (int j = -MH; j < MH; ++j)
+                        for (int i = -MW; i < MW; ++i)
+                            if (mask[i + MW, j + MH] == 1 && src[x + i, y + j] < min)
+                                min = src[x + i, y + j];
+                    dest[x, y] = min;
+                }
+            return dest;
+        }
+
+        public static int[,] Closing(int[,] src, int[,] mask) {
+            return Erode(Dilate(src,mask), mask);
+        }
+
+        public static int[,] Opening(int[,] src, int[,] mask){
+            return Dilate(Erode(src, mask), mask);
+        }
+
+        public static int[,] Or(int[,] first, int[,] second) {
+            var W = first.GetLength(0);
+            var H = first.GetLength(1);
+            var dest = new int[W, H];
+
+            for (int x = 0; x < W; ++x)
+                for (int y = 0; y < H; ++y)
+                    dest[x, y] = first[x, y] == 1 || second[x, y] == 1 ? 1 : 0;
+            return dest;
+        }
+
+        public static int[,] And(int[,] first, int[,] second)
+        {
+            var W = first.GetLength(0);
+            var H = first.GetLength(1);
+            var dest = new int[W, H];
+
+            for (int x = 0; x < W; ++x)
+                for (int y = 0; y < H; ++y)
+                    dest[x, y] = first[x, y] == 1 && second[x, y] == 1 ? 1 : 0;
+            return dest;
+        }
+
+        public static int[,] Difference(int[,] first, int[,] second)
+        {
+            var W = first.GetLength(0);
+            var H = first.GetLength(1);
+            var dest = new int[W, H];
+
+            for (int x = 0; x < W; ++x)
+                for (int y = 0; y < H; ++y)
+                    dest[x, y] = first[x, y] == 1 && second[x, y] == 0 ? 1 : 0;
+            return dest;
+        }
+    }
 }
